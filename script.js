db0367282e85ea167fbff103359999c5e9284403
jeandertrevia <jeandertrevia305@gmail.com>
2023-02-01 08:57:04 -0300

    FixLogica

--- script.js
+++ script.js
@@ -15,37 +15,19 @@ function showSlider() {
 function nextSlider() {
     if(currentSlide < slider.length -1) {
         currentSlide++
-<<<<<<< HEAD
         hideSlider()
         showSlider()
     }
-=======
-    hideSlider()
-    showSlider()
->>>>>>> 1a1d6d94c42fde4f23daf0318265c58b7593f657
-}
 }
 
 function prevSlider() {
     hideSlider()
-<<<<<<< HEAD
     if (currentSlide > 0) {
         currentSlide--
     }
-=======
-    if(currentSlide > 0) {
-        currentSlide--} 
-   
->>>>>>> 1a1d6d94c42fde4f23daf0318265c58b7593f657
     showSlider()
 }
 
     btnNext.addEventListener('click', nextSlider)
     btnPrev.addEventListener('click', prevSlider)
 
-<<<<<<< HEAD
-    
-=======
-    console.log(slider)
-        
->>>>>>> 1a1d6d94c42fde4f23daf0318265c58b7593f657
